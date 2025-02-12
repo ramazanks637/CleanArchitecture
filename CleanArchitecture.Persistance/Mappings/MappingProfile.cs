3e98c55caeb63782c0834a81f51009ec5a81cb04
ramazankose <ramazanks637@gmail.com>
2025-02-13 02:14:29 +0300

    CQRS Pattern , Dependency Injection Pattern, AutoMapper

---
+++ CleanArchitecture.Persistance/Mappings/MappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using CleanArchitecture.Application.Feature.CarFeatures.Commands.CreateCar;
+using CleanArchitecture.Domain.Entities;
+
+namespace CleanArchitecture.Persistance.Mappings
+{
+    public sealed class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<CreateCarCommand, Car>().ReverseMap();
+        }
+    }
+}
--- CleanArchitecture.WebApi/Program.cs
+++ CleanArchitecture.WebApi/Program.cs
@@ -1,8 +1,14 @@
+using CleanArchitecture.Application.Services;
 using CleanArchitecture.Persistance.Context;
+using CleanArchitecture.Persistance.Services;
 using Microsoft.EntityFrameworkCore;
 
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddScoped<ICarService, CarService>();
+
+builder.Services.AddAutoMapper(typeof(CleanArchitecture.Persistance.AssemblyReferance).Assembly);
 
 
 
