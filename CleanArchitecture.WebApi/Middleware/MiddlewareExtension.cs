bf4cb7104ce66ee32f41a8bf4998c71c1334f40d
ramazankose <ramazanks637@gmail.com>
2025-02-13 03:20:16 +0300

    Exception Middleware

---
+++ CleanArchitecture.WebApi/Middleware/MiddlewareExtension.cs
@@ -0,0 +1,11 @@
+﻿namespace CleanArchitecture.WebApi.Middleware
+{
+    public static class MiddlewareExtension
+    {
+        public static IApplicationBuilder UseMiddlewareExtansion(this IApplicationBuilder app)
+        {
+            app.UseMiddleware<ExceptionMiddleware>();
+            return app;
+        }
+    }
+}
